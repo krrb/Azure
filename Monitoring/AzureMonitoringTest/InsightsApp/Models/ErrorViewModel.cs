1dd60b1cc11ba83b599cddc2717780b5c32e87a4
Rajeev Banda <rajeev.banda@health.vic.gov.au>
2022-06-07 20:17:27 +1000

    AppInsight sample

---
+++ Monitoring/AzureMonitoringTest/InsightsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InsightsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ RedisTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+namespace BlobTest
+{
+    class Program
+    {
+        private static readonly string connectionString = "rns-redis-001.redis.cache.windows.net:6380,password=YcxyCBq88Sok59ks3c7hZqiMZiMkDJyKTAzCaPa1RBc=,ssl=True,abortConnect=False";
+        
+        static async Task Main()
+        { 
+            using (var cache = ConnectionMultiplexer.Connect(connectionString))
+            {
+                IDatabase db = cache.GetDatabase();
+
+                var result = await db.ExecuteAsync("ping");
+                Console.WriteLine($"PING = {result.Type} : {result}");
+
+                bool setValue = await db.StringSetAsync("test:key", "100");
+                Console.WriteLine($"SET : {setValue}");
+
+                string getValue = await db.StringGetAsync("test:key");
+                Console.WriteLine($"GET : {getValue}");
+            }
+        }
+    }
+}
